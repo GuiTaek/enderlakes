5bba5505b0ec18ac5bca634151ee6402c3eec685
GuiTaekM <guitaekm@gmail.com>
2024-08-05 23:50:49 +0200

    make a minecraft project

---
+++ src/client/java/com/gmail/guitaekm/enderlakes/EnderlakesClient.java
@@ -0,0 +1,10 @@
+package com.gmail.guitaekm.enderlakes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderlakesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
